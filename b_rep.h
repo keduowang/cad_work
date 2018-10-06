34d1403ccc2221b694873366fb02285ab4892cfc
Fu Kejie <fukejie@MyMacBookPro.local>
2018-10-06 14:36:57 +0800

    finish b_rep.h

---
+++ b_rep.h
@@ -0,0 +1,112 @@
+#ifndef B_REP_H
+#define B_REP_H
+
+#include <stdlib.h>
+#include <vector>
+class Vertex;
+class HalfEdge;
+class Edge;
+class Loop;
+class Face;
+class Solid;
+
+class Point;
+class Curve;
+class Surface;
+
+/* Topological class: Vertex */
+class Vertex
+{
+public:
+    Vertex(): vPoint(nullptr), nxtVertex(nullptr), prevVertex(nullptr){}
+    Point* vPoint;
+    Vertex* nxtVertex;
+    Vertex* prevVertex;
+};
+
+/* Topological class: HalfEdge */
+class HalfEdge
+{
+public:
+    HalfEdge(): heVertex(nullptr), heLoop(nullptr), heEdge(nullptr), prevHalfEdge(nullptr), nxtHalfEdge(nullptr){}
+    Vertex* heVertex;
+    Loop* heLoop;
+    Edge* heEdge; /* Map HalfEdge to Edge */
+    HalfEdge* prevHalfEdge;
+    HalfEdge* nxtHalfEdge;
+};
+
+
+/* Topological class: Edge */
+class Edge
+{
+public:
+    Edge(): eHalfEdge1(nullptr), eHalfEdge2(nullptr), prevEdge(nullptr){}
+    HalfEdge* eHalfEdge1;
+    HalfEdge* eHalfEdge2;
+    Edge* prevEdge;
+    Edge* nxtEdge;
+};
+
+
+/* Topological class: Loop */
+class Loop
+{
+public:
+    Loop(): lHalfEdge(nullptr), lFace(nullptr), prevLoop(nullptr), nxtLoop(nullptr){}
+    HalfEdge* lHalfEdge;
+    Face* lFace;
+    Loop* prevLoop;
+    Loop* nxtLoop;
+
+};
+
+/* Topological class: Face */
+class Face
+{
+public:
+    Face(): fSolid(nullptr), fLoops(nullptr), prevFace(nullptr), nxtFace(nullptr){}
+    Solid* fSolid;
+    Loop* fLoops;
+    Face* prevFace;
+    Face* nxtFace;
+};
+
+
+/* Topological class: Solid */
+class Solid
+{
+public:
+    Solid(): sFaces(nullptr), sEdges(nullptr), prevSolid(nullptr), nxtSolid(nullptr){}
+    Face* sFaces;
+    Edge* sEdges;
+    Solid* prevSolid;
+    Solid* nxtSolid;
+};
+
+
+/*
+ *
+*/
+
+/* Geometry class: Point */
+class Point
+{
+public:
+    double pos[3];
+};
+
+/*Geometry class: Curve*/
+class  Curve
+{
+
+};
+
+/*Geometry class: Surface*/
+class Surface
+{
+
+};
+
+
+#endif // B_REP_H
---
+++ euler_op.h
@@ -0,0 +1,4 @@
+#ifndef EULER_OP_H
+#define EULER_OP_H
+
+#endif // EULER_OP_H
